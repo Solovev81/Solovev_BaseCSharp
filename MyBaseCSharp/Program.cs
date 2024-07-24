4bed46553d881980ca8ea9b1553fdeaea5897feb
Артем Соловьев <a.solovev@dunice.net>
2024-07-24 10:41:38 +0300

    added filesName in .gitignor and made Task1Job5

--- MyBaseCSharp/Program.cs
+++ MyBaseCSharp/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//---------- Task1_job5
+using MyBaseCSharp.Task1;
+
+TaskJob5 taskJob5 = new TaskJob5();
+taskJob5.StartInstallation();
+//----------
+
+
+
+// Создать консольное приложение “Корзина”, в “Корзину” можно добавлять
+// продукты а затем выводить их все в консоль.
+// List<string> products = new List<string>() {"apple","milk","meat"};
+
+// Basket basket1 = new Basket();
+// Basket basket2 = new Basket();
+
+// basket1.PrintAllProducts();
+//System.Console.WriteLine();
+
+// foreach(var prod in products)
+// {
+//     basket2.AddNewProduct(prod);
+// }
+
+// basket2.PrintAllProducts();
+
+// class Basket {
+//     private List<string> _productsBasket = new List<string>();
+
+//     public void AddNewProduct (string newProd)
+//     {
+//         _productsBasket.Add(newProd);
+//         System.Console.WriteLine($"Вы добавили {newProd}");
+//     }
+
+//     public void PrintAllProducts()
+//     {
+//         if (!_productsBasket.Any())
+//         {
+//             System.Console.WriteLine("Амбар пуст, Милорд");
+//         }
+//         else
+//         {
+//             System.Console.WriteLine("Запасы провизии пополняются, Сэр");
+//             foreach (var prod in _productsBasket)
+//             {
+//                 System.Console.WriteLine(prod);
+//             }
+//             System.Console.WriteLine("Люди любят вас, мой Лорд");
+//         }
+//     }
+// }
+
+// Создать консольное приложение, в котором можно задать минимальное
+// и максимальное число (тип int), после чего в консоль выводятся все
+// простые числа в указанном промежутке.
--- MyBaseCSharp/Task1/TaskJob5.cs
+++ MyBaseCSharp/Task1/TaskJob5.cs
@@ -7,6 +7,39 @@ namespace MyBaseCSharp.Task1
 {
     public class TaskJob5
     {
-        
+        // Создать консольное приложение, которое при старте попросит указать
+        // пароль и далее будет спрашивать его, пока не будет введен верный,
+        // после ввода верного пароля программа закончит свое выполнение, а при
+        // вводе некорректного пароля будет просьба ввести пароль еще раз.
+
+        public void StartInstallation()
+        {
+            Console.WriteLine("Установка системы...");
+
+            Console.Write("Создайте пароль: ");
+            string? myPassword = Console.ReadLine();
+
+            Console.Clear();
+
+            Login(myPassword);
+        }
+
+        public void Login(string? myPassword)
+        {
+            while (true)
+            {
+                Console.WriteLine("Добро пожаловать User!");
+
+                Console.Write("Введи пароль: ");
+                string? somePassword = Console.ReadLine();
+
+                if (somePassword == myPassword)
+                {
+                    break;
+                }
+
+                Console.Clear();
+            };
+        }
     }
 }
