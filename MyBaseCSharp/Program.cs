90201d998ee2bcabb8aad17d45dae9e1ea292b1e
Артем Соловьев <a.solovev@dunice.net>
2024-07-24 10:49:23 +0300

    made Task1Job4

--- MyBaseCSharp/Program.cs
+++ MyBaseCSharp/Program.cs
@@ -1,56 +1,27 @@
-﻿//---------- Task1_job5
-using MyBaseCSharp.Task1;
+﻿using MyBaseCSharp.Task1;
 
-TaskJob5 taskJob5 = new TaskJob5();
-taskJob5.StartInstallation();
+//---------- Task1_job5
+// TaskJob5 taskJob5 = new TaskJob5();
+// taskJob5.StartInstallation();
 //----------
 
+//---------- Task1_job4
+// List<string> products = new List<string>() { "apple", "milk", "meat" };
 
-
-// Создать консольное приложение “Корзина”, в “Корзину” можно добавлять
-// продукты а затем выводить их все в консоль.
-// List<string> products = new List<string>() {"apple","milk","meat"};
-
-// Basket basket1 = new Basket();
-// Basket basket2 = new Basket();
+// TaskJob4 basket1 = new TaskJob4();
+// TaskJob4 basket2 = new TaskJob4();
 
 // basket1.PrintAllProducts();
-//System.Console.WriteLine();
+// Console.WriteLine();
 
-// foreach(var prod in products)
+// foreach (var prod in products)
 // {
 //     basket2.AddNewProduct(prod);
 // }
 
 // basket2.PrintAllProducts();
+//----------
 
-// class Basket {
-//     private List<string> _productsBasket = new List<string>();
+//---------- Task1_job3
 
-//     public void AddNewProduct (string newProd)
-//     {
-//         _productsBasket.Add(newProd);
-//         System.Console.WriteLine($"Вы добавили {newProd}");
-//     }
-
-//     public void PrintAllProducts()
-//     {
-//         if (!_productsBasket.Any())
-//         {
-//             System.Console.WriteLine("Амбар пуст, Милорд");
-//         }
-//         else
-//         {
-//             System.Console.WriteLine("Запасы провизии пополняются, Сэр");
-//             foreach (var prod in _productsBasket)
-//             {
-//                 System.Console.WriteLine(prod);
-//             }
-//             System.Console.WriteLine("Люди любят вас, мой Лорд");
-//         }
-//     }
-// }
-
-// Создать консольное приложение, в котором можно задать минимальное
-// и максимальное число (тип int), после чего в консоль выводятся все
-// простые числа в указанном промежутке.
+//----------
--- MyBaseCSharp/Task1/TaskJob4.cs
+++ MyBaseCSharp/Task1/TaskJob4.cs
@@ -5,8 +5,35 @@ using System.Threading.Tasks;
 
 namespace MyBaseCSharp.Task1
 {
-    public class TaskJob4
+    public class TaskJob4 //Basket
     {
+        // Создать консольное приложение “Корзина”, в “Корзину” можно добавлять
+        // продукты а затем выводить их все в консоль.
+
+        private List<string> _productsBasket = new List<string>();
+
+        public void AddNewProduct(string newProd)
+        {
+            _productsBasket.Add(newProd);
+            Console.WriteLine($"Вы добавили {newProd}");
+        }
+
+        public void PrintAllProducts()
+        {
+            if (!_productsBasket.Any())
+            {
+                Console.WriteLine("Амбар пуст, Милорд");
+            }
+            else
+            {
+                Console.WriteLine("Запасы провизии пополняются, Сэр");
+                foreach (var prod in _productsBasket)
+                {
+                    Console.WriteLine(prod);
+                }
+                Console.WriteLine("Люди любят вас, мой Лорд");
+            }
+        }
 
     }
 }
