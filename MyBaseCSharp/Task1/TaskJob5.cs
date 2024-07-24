8d7348526394d638046912a22f425dde18127971
Артем Соловьев <a.solovev@dunice.net>
2024-07-24 10:13:08 +0300

    start to work

---
+++ MyBaseCSharp/Task1/TaskJob5.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBaseCSharp.Task1
+{
+    public class TaskJob5
+    {
+        
+    }
+}
