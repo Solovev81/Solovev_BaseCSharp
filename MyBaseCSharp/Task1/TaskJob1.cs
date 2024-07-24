eda72eb87767890d29c4eb7c6e3b1c1759cec44d
Артем Соловьев <a.solovev@dunice.net>
2024-07-24 12:37:52 +0300

    refactoring job1

--- MyBaseCSharp/Task1/TaskJob1.cs
+++ MyBaseCSharp/Task1/TaskJob1.cs
@@ -11,9 +11,14 @@ namespace MyBaseCSharp.Task1
         // для примитивов). Ожидаются переменные типов bool, byte, int, long,
         // float, double, char, string. Также необходимо вывести в консоль
         // массив int-чисел (Array<int>) и тип object.
+
+        int[] _myIntArray = { 1, 2, 3, 4 };
+        List<int> _myIntList = new List<int>() { 1, 2, 3, 4, 5 };
+        object _myObject = new object();
+
         public void GetAllMaxMinValueOfTypes()
         {
-            Console.WriteLine($"bool Минимальное значение: {false} Максимальное значение: {true}");
+            Console.WriteLine($"bool Минимальное значение: {false}/0 Максимальное значение: {true}/1");
             Console.WriteLine($"byte Минимальное значение: {byte.MinValue} Максимальное значение: {byte.MaxValue}");
             Console.WriteLine($"int Минимальное значение: {int.MinValue} Максимальное значение: {int.MaxValue}");
             Console.WriteLine($"long Минимальное значение: {long.MinValue} Максимальное значение: {long.MaxValue}");
@@ -21,24 +26,18 @@ namespace MyBaseCSharp.Task1
             Console.WriteLine($"double Минимальное значение: {double.MinValue} Максимальное значение: {double.MaxValue}");
             Console.WriteLine($"char Минимальное значение: {char.MinValue} Максимальное значение: {char.MaxValue}");
             Console.WriteLine("string Минимальное значение: 0 or null Максимальное значение: {-}");
+            Console.WriteLine(_myObject.GetType());
+            Enumerator(_myIntArray);
+            Console.WriteLine("-------");
+            Enumerator(_myIntList.ToArray());
+        }
 
-            int[] myInt = { 1, 2, 3, 4 };
-            List<int> myArray = new List<int>();
-            object myObject = new object();
-
-            foreach (int i in myInt)
+        private void Enumerator(Array array)
+        {
+            foreach (var a in array)
             {
-                myArray.Add(i);
-                Console.WriteLine(i);
-                Console.WriteLine();
+                Console.WriteLine(a);
             }
-
-            foreach (int i in myArray)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine(myObject.GetType());
         }
     }
 }
